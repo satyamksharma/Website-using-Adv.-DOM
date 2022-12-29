10a413ae21b272ad77289947da1b87cd7d59a7dc
Satyam Kumar <ksatyam433@gmail.com>
2022-12-29 21:48:30 +0530

    Smooth scrolling in navbar added

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
     e.preventDefault();
@@ -27,4 +29,21 @@ document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
+});
+
+////////////////////////////
+// Scrolling
+
+btnScrollTo.addEventListener('click', function (e) {
+    section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page Navigation
+
+document.querySelectorAll('.nav__link').forEach(function (el) {
+    el.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = this.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    });
 });
